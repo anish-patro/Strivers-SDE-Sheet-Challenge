ec6c3becfc55788347e21995a6b1f28c1920b473
G.Anish Kumar Patro <105335549+anish-patro@users.noreply.github.com>
2023-06-08 04:07:23 +0530

    Add files via upload

---
+++ Day 2/majority elment.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int findMajorityElement(int arr[], int n) {
+	// Write your code here.
+	map<int, int>m;
+	for(int i=0;i<n; i++)
+	m[arr[i]]++;
+	for(auto it:m)
+	if(it.second> floor(n/2)) return it.first;
+	return -1;
+	
+}
