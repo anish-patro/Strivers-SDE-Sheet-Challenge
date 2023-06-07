619f4c7a8b473afc90c88109467d6c397bb97171
G.Anish Kumar Patro <105335549+anish-patro@users.noreply.github.com>
2023-06-08 00:35:12 +0530

    Add files via upload

---
+++ Day 2/merge intervals.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+/*
+
+    intervals[i][0] = start point of i'th interval
+    intervals[i][1] = finish point of i'th interval
+
+*/
+
+vector<vector<int>> mergeIntervals(vector<vector<int>> &intervals)
+{
+    // Write your code here.
+    sort(intervals.begin(), intervals.end());
+    
+    vector<vector<int>>ans;
+     if(intervals.size()==0)return ans;
+    ans.push_back(intervals[0]);
+    int j=0;
+    for(int i=1;i<intervals.size();i++){
+        if(intervals[i][0]<=ans[j][1])
+        ans[j][1]=max(ans[j][1], intervals[i][1]);
+        else{
+            j++;
+            ans.push_back(intervals[i]);
+        }
+    }
+    return ans;
+}
