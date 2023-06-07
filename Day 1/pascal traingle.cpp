e5c8209cf0f446abb4f2c0c2c3725cb539b52c2c
G.Anish Kumar Patro <105335549+anish-patro@users.noreply.github.com>
2023-06-07 21:53:40 +0530

    Add files via upload

---
+++ Day 1/pascal traingle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+vector<vector<long long int>> printPascal(int n) 
+{
+  // Write your code here.
+  vector<vector<long long int>>res;
+  for(int i=0;i<n;i++){
+    vector<long long int>temp(i+1, 1);
+    for(int j=1;j<i;j++)
+    temp[j]=res[i-1][j-1]+res[i-1][j];
+    res.push_back(temp);
+  }
+  return res;
+}
