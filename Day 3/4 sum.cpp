75b4791b229342a10d275ae55cd894b6c3c8ce87
G.Anish Kumar Patro <105335549+anish-patro@users.noreply.github.com>
2023-06-09 14:15:26 +0530

    Add files via upload

---
+++ Day 3/4 sum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+string fourSum(vector<int> nums, int target, int n) {
+    // Write your code here.
+     sort(nums.begin(),nums.end());
+       for(int i=0;i<n-3;++i){
+           for(int j=i+1;j<n-2;++j){
+               long long newTarget=(long long)target-(long long)nums[i]-(long long)nums[j];
+               int low=j+1;
+               int high=n-1;
+               while(low<high){
+                   if(nums[low]+nums[high]<newTarget)
+                   low++;
+                   else if(nums[low]+nums[high]>newTarget)
+                   high--;
+                   else
+                   {
+                 return "Yes";
+                   }
+               }
+               
+           }
+ 
+       }
+       return "No";
+}
