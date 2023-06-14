67d006c2b8008b94a885942dfdbd981fccb6693a
G.Anish Kumar Patro <105335549+anish-patro@users.noreply.github.com>
2023-06-15 02:52:00 +0530

    Add files via upload

---
+++ Day 6/ll cycle 2.cpp
@@ -0,0 +1,52 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+Node *firstNode(Node *head)
+{
+    //    Write your code here.
+    if(head==NULL || head->next==NULL) return NULL;
+    Node* temp=head;
+    Node* slow=head;
+    Node* fast=head;
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            Node* curr=fast;
+            while(temp!=fast){
+                fast=fast->next;
+                temp=temp->next;
+               
+            }
+            return temp;
+        }
+    }
+    return NULL;
+    
+}
