8453edc2af4e55598cedc71c158c6929aa415e65
G.Anish Kumar Patro <105335549+anish-patro@users.noreply.github.com>
2023-07-14 18:06:09 +0530

    Add files via upload

---
+++ Day 12/kth perm.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+string kthPermutation(int n, int k) {
+   int fact=1;
+        vector<int>arr;
+
+        for(int i=1;i<n;++i){
+            fact*=i;
+            arr.push_back(i);
+        }
+       arr.push_back(n);
+       k=k-1;
+        string ans="";
+
+        while(true){
+            ans=ans+to_string(arr[k/fact]);
+            arr.erase(arr.begin()+k/fact);
+            if(arr.size()==0) break;
+
+            k%=fact;
+            fact/=arr.size();
+        }
+        return ans;
+}
